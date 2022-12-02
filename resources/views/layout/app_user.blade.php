b99eff40aaa6888ef758278f3bd53e38140ed2a1
ctgrace <grace@cashtree.id>
2022-12-02 13:24:02 +0700

    favicon

--- resources/views/layout/app_user.blade.php
+++ resources/views/layout/app_user.blade.php
@@ -7,6 +7,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <link rel="icon" href="{{url('favicon.jpg')}}">
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
